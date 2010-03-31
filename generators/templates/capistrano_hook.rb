3efb932dd4f384a60ddaec86f489dd2b4804fbe2
pjaspers <junkiesxl@gmail.com>
2010-03-31 21:38:17 +0200

    Removed vendoring-stuff. Capfire is not supposed to be vendored.

--- generators/templates/capistrano_hook.rb
+++ generators/templates/capistrano_hook.rb
@@ -1,7 +1,3 @@
-Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'capfire-*')].each do |vendored_notifier|
-  $: << File.join(vendored_notifier, 'lib')
-end
-
 begin
   require 'capfire/capistrano'
 rescue Object
